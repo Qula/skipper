ea0f50f1ecdb93f5200363b9f7fd89ce7cbd2b1b
Qula <kulamarcin2@gmail.com>
2016-03-25 16:58:28 +0100

    Bootstrap

--- index.php
+++ index.php
@@ -2,10 +2,17 @@
 <html lang="pl">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Aktualności</title>
+
+    <link rel="stylesheet" href="bootstrap336/css/bootstrap.min.css">
+
+
 </head>
 <body>
-
+    <h1>Aktualności</h1>
+    <script src="bootstrap336/js/jquery-1.12.2.min.js"></script>
+    <script src="bootstrap336/js/bootstrap.min.js"></script>
 </body>
 </html>
 
