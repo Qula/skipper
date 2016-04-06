5061022679ae5aa32b35d7d01371642782876d7c
Qula <kulamarcin2@gmail.com>
2016-04-06 23:59:04 +0200

    reCaptcha

--- login.php
+++ login.php
@@ -1,19 +1,25 @@
 <?php include 'header.php'; ?>
+    <script src='https://www.google.com/recaptcha/api.js'></script>
 <div class="row">
     <div class="col-md-3 col-md-offset-5 col-sm-3 col-sm-offset-5 col-xs-5 col-xs-offset-4">
         <form action="user-manager.php?command=login" method="post">
             <input id="login" type="text" class="form-control" name="login" placeholder="Nazwa użytkownika" required>
             <input id="password" type="password" class="form-control" name="password" placeholder="Hasło" required>
+            <div class="g-recaptcha" data-sitekey="6LfGxBwTAAAAAGTdagt32GKPtufgym1IzgE7UPyu"></div>
             <?php
             if (isset($_GET['error'])) {
                 $error = $_GET['error'];
                 if ($error == 'loginError') {
                     echo '<br><p>Niepoprawna nazwa użytkownika lub hasło!</p>';
+                }else if($error == 'recaptchaError'){
+                    echo '<br><p>Błąd reCaptcha!</p>';
                 }
             }
             ?>
-                <button class="btn btn-primary">Zaloguj</button>
+
+            <button class="btn btn-primary">Zaloguj</button>
         </form>
     </div>
 </div>
+
 <?php include 'footer.php'; ?>
--- user-manager.php
+++ user-manager.php
@@ -6,19 +6,36 @@ include 'model.php';
 $command = $_GET['command'];
 
 if($command === 'login') {
-    $login = $_POST['login'];
-    $password = $_POST['password'];
+    if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])){
+        $captcha=$_POST['g-recaptcha-response'];
+        $secretKey = "6LfGxBwTAAAAAFycodYmMbqdZXZSnZyvQ2_CGCqd";
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip);
+        $responseKeys = json_decode($response,true);
+        echo $responseKeys['success'];
+        if($responseKeys['success'] === true ) {
+            $login = $_POST['login'];
+            $password = $_POST['password'];
 
-    $model = Model::getInstance();
-    $result = $model->getUser($login, $password);
-
-    if ($result->num_rows > 0) {
-        session_start();
-        $_SESSION['login'] = $login;
-        header('Location: admin.php');
-    } else {
-        header('Location: login.php?error=loginError');
+            $model = Model::getInstance();
+            $result = $model->getUser($login, $password);
+            if ($result->num_rows > 0) {
+                session_start();
+                $_SESSION['login'] = $login;
+                header('Location: admin.php');
+            } else {
+                header('Location: login.php?error=loginError');
+            }
+        }else{
+            header('Location: login.php?error=recaptchaError');
+        }
     }
+    else{
+        header('Location: login.php?error=recaptchaError');
+    }
+
+
+
 } else if ($command == 'logout') {
     session_start();
     session_destroy();
